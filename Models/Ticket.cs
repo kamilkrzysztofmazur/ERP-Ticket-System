27c25e1ce4f1955f91d411d4ccd45bdc3c95f2c1
Kamil Mazur <kamilkrzysztofmazur@gmail.com>
2026-05-06 11:33:34 +0200

    refactor: improve ticket model and service structure

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,18 +1,29 @@
 using System;
 
-namespace ERP_Ticket_System.Models{
-    public class Ticket {
 
-        
+namespace ERP_Ticket_System.Models
+{
+    public class Ticket
+    {
+
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
 
-        public Ticket(string Title, string Description, string Status){
-            
+        public Ticket(string Title, string Description, string Status)
+        {
+
             this.Title = Title;
             this.Description = Description;
             this.Status = Status;
-            }
+        }
+        public Ticket(int Id, string Title, string Description, string Status)
+        {
+            this.Id = Id;
+            this.Title = Title;
+            this.Description = Description;
+            this.Status = Status;
         }
     }
+}
--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -3,21 +3,25 @@ using DBConnection;
 using ERP_Ticket_System.Models;
 using Microsoft.Data.SqlClient;
 
-namespace ERP_Ticket_System.Services{
-    public class TicketService{
-        
-        public void AddTicket(Ticket ticket){
+namespace ERP_Ticket_System.Services
+{
+    public class TicketService
+    {
+
+        public void AddTicket(Ticket ticket)
+        {
 
             SqlConnection connection = new SqlConnection(Connection.configuration);
-            
-            string query = "INSERT INTO Ticket (Title, Description, Status) VALUES (@Title, @Description, @Status)";
-                            
-            using (SqlCommand command = new SqlCommand(query, connection)){
 
-                command.Parameters.AddWithValue("@Title",ticket.Title);
-                command.Parameters.AddWithValue("@Description",ticket.Description);
+            string query = "INSERT INTO Ticket (Title, Description, Status) VALUES (@Title, @Description, @Status)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+
+                command.Parameters.AddWithValue("@Title", ticket.Title);
+                command.Parameters.AddWithValue("@Description", ticket.Description);
                 command.Parameters.AddWithValue("@Status", ticket.Status);
-                
+
                 connection.Open();
 
                 command.ExecuteNonQuery();
