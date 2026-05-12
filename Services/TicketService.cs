6423fc0ef412223b65762490cef635d7a8b33fd0
Kamil Mazur <kamilkrzysztofmazur@gmail.com>
2026-05-12 12:16:21 +0200

    feat: add GetAllTickets method

--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -2,6 +2,7 @@ using System;
 using DBConnection;
 using ERP_Ticket_System.Models;
 using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace ERP_Ticket_System.Services
 {
@@ -11,23 +12,54 @@ namespace ERP_Ticket_System.Services
         public void AddTicket(Ticket ticket)
         {
 
-            SqlConnection connection = new SqlConnection(Connection.configuration);
+            using (SqlConnection connection = new SqlConnection(Connection.configuration))
+                {
+                string query = "INSERT INTO Ticket (Title, Description, Status) VALUES (@Title, @Description, @Status)";
 
-            string query = "INSERT INTO Ticket (Title, Description, Status) VALUES (@Title, @Description, @Status)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                    {
 
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
+                        command.Parameters.AddWithValue("@Title", ticket.Title);
+                        command.Parameters.AddWithValue("@Description", ticket.Description);
+                        command.Parameters.AddWithValue("@Status", ticket.Status);
 
-                command.Parameters.AddWithValue("@Title", ticket.Title);
-                command.Parameters.AddWithValue("@Description", ticket.Description);
-                command.Parameters.AddWithValue("@Status", ticket.Status);
+                        connection.Open();
 
-                connection.Open();
-
-                command.ExecuteNonQuery();
-            }
-            connection.Close();
+                        command.ExecuteNonQuery();
+                    
+                        connection.Close();
+                    }
+                }
         }
+        public List<Ticket> GetAllTickets()
+            {
+            List<Ticket> tickets = new List<Ticket>();
+
+            string query = "SELECT id, Title, Description, Status FROM Ticket";
+            
+            using (SqlConnection connection = new SqlConnection(Connection.configuration))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                        using(SqlDataReader reader = command.ExecuteReader())
+                            {
+                            while (reader.Read())
+                                {
+                                int id = reader.GetInt32(0);
+                                string title = reader.GetString(1);
+                                string description = reader.GetString(2);
+                                string status = reader.GetString(3);
+
+                                Ticket ticket = new Ticket(id, title,description, status);
+                                
+                                tickets.Add(ticket);
+
+                                }
+                            }
+                        }
+                }
+            return tickets;
+            }
     }
 
 }
